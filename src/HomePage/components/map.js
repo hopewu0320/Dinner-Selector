703e5bcad6f37555f7b51cb6f0564fe5b7d0eb64
hopewu0320 <gccbs01596@gmail.com>
2023-11-04 16:57:06 +0800

    繪製map 顯示距離

--- src/HomePage/components/map.js
+++ src/HomePage/components/map.js
@@ -4,15 +4,22 @@ const MapContainer = () => {
   const googleMapRef = useRef(null);
   let googleMap = null;
 
-  // 初始化地图
   useEffect(() => {
     if (window.google) {
       const initializeMap = () => {
         const mapOptions = {
           zoom: 8,
-          center: { lat: 25.0196468, lng: 121.4134637 }, // 示例中心点，根据需要替换
+          center: { lat: -34.397, lng: 150.644 },
         };
+
         googleMap = new window.google.maps.Map(googleMapRef.current, mapOptions);
+
+        // 创建DirectionsService和DirectionsRenderer实例
+        const directionsService = new window.google.maps.DirectionsService();
+        const directionsRenderer = new window.google.maps.DirectionsRenderer();
+        directionsRenderer.setMap(googleMap);
+
+        calculateAndDisplayRoute(directionsService, directionsRenderer);
       };
 
       initializeMap();
@@ -21,7 +28,28 @@ const MapContainer = () => {
     }
   }, []);
 
-  return <div ref={googleMapRef} style={{ width: '400px', height: '300px' }} />;
+  // 计算和显示路线
+  const calculateAndDisplayRoute = (directionsService, directionsRenderer) => {
+    const start = { lat: 25.0196468, lng: 121.4134637 }; // 起点坐标
+    const end = { lat: 25.026144, lng: 121.419915 }; // 终点坐标
+    directionsService.route(
+      {
+        origin: start,
+        destination: end,
+        travelMode: window.google.maps.TravelMode.DRIVING, // 可以更改为WALKING, BICYCLING, 或TRANSIT
+      },
+      (response, status) => {
+        if (status === window.google.maps.DirectionsStatus.OK) {
+          // 显示路线
+          directionsRenderer.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      }
+    );
+  };
+
+  return <div ref={googleMapRef} style={{ width: '100%', height: '400px' }} />;
 };
 
 export default MapContainer;
